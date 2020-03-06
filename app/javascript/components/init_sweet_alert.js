0b6214a3c2f8d29031ad4e2cb9ef70db449ebf72
Estelle Boujenah <boujenahestelle@hotmail.fr>
2020-03-06 16:06:29 +0100

    done

--- app/javascript/components/init_sweet_alert.js
+++ app/javascript/components/init_sweet_alert.js
@@ -3,10 +3,9 @@ import Swal from 'sweetalert2'
 
 const initSweetAlert = () => {
   const button = document.querySelector('.sweet-alert');
-  button.addEventListener('click', (event) => {
+  button.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(event.currentTarget);
-    console.log("ok");
+    Swal.fire('Oops...', 'Something went wrong!', 'error')
   });
 };
 
