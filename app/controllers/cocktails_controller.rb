1bf663074c469a8d7b10cbf10481b5d6dcb16ded
Estelle Boujenah <boujenahestelle@hotmail.fr>
2020-03-06 23:28:43 +0100

    add carousel

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -24,6 +24,6 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo)
+    params.require(:cocktail).permit(:name, photos: [])
   end
 end
